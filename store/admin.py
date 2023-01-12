63b34f3000a776d6041cd82ca63bceff82efcd33
Nursultan301 <abykeevnursultan14@gmail.com>
2023-01-12 23:10:32 +0600

    Installed djnagorestframework

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField("Называние", max_length=100)
+    price = models.DecimalField("Цена", max_digits=7, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
+
+    def __str__(self):
+        return self.name
