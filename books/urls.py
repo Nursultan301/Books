54111f34950e5039a2cc0baad2f38b53298e7455
Nursultan301 <abykeevnursultan14@gmail.com>
2023-01-12 23:33:29 +0600

    wrote the sterilizer

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from store.views import BookViewSet
+
+router = SimpleRouter()
+
+router.register(r'book', BookViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from store.models import Book
+from store.serializers import BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
