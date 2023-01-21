6d29e8436e3e6e4b540d47250d3800b8329e2d24
Nursultan301 <abykeevnursultan14@gmail.com>
2023-01-21 16:57:42 +0600

    Add test Filters

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -7,12 +7,33 @@ from store.serializers import BookSerializer
 
 
 class BookApiTestCage(APITestCase):
+    def setUp(self):
+        self.book_1 = Book.objects.create(name='Test book 1', price=25,
+                                          author_name='Author 1')
+        self.book_2 = Book.objects.create(name='Test book 2', price=55,
+                                          author_name='Author 5')
+        self.book_3 = Book.objects.create(name='Test book Author 1', price=55,
+                                          author_name='Author 2')
+
     def test_get(self):
-        book_1 = Book.objects.create(name='Test book 1', price=1000)
-        book_2 = Book.objects.create(name='Test book 2', price=2000)
         url = reverse('book-list')
         response = self.client.get(url)
-        serializer_data = BookSerializer([book_1, book_2], many=True).data
+        serializer_data = BookSerializer([self.book_1, self.book_2, self.book_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
-        print(response.data)
+
+    def test_get_filter(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'price': 55})
+        serializer_data = BookSerializer([self.book_2,
+                                          self.book_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_search(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'search': 'Author 1'})
+        serializer_data = BookSerializer([self.book_1,
+                                          self.book_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
