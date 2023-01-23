23a709746d5206963d9ff3e026d8c9d758559167
Nursultan301 <abykeevnursultan14@gmail.com>
2023-01-23 09:17:07 +0600

    Permissions owner and staff

--- store/tests/test_serializer.py
+++ store/tests/test_serializer.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from store.models import Book
@@ -6,21 +7,25 @@ from store.serializers import BookSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
-        book_1 = Book.objects.create(name='Test book 1', price=1000, author_name='Author 1')
-        book_2 = Book.objects.create(name='Test book 2', price=2000, author_name='Author 2')
+        self.user = User.objects.create(username='test_username')
+        book_1 = Book.objects.create(name='Test book 1', price=1000, author_name='Author 1', owner=self.user)
+        book_2 = Book.objects.create(name='Test book 2', price=2000, author_name='Author 2', owner=self.user)
         data = BookSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Test book 1',
                 'price': '1000.00',
-                'author_name': 'Author 1'
-              },
+                'author_name': 'Author 1',
+                "owner": book_1.owner.id
+            },
             {
                 'id': book_2.id,
                 'name': 'Test book 2',
                 'price': '2000.00',
-                'author_name': 'Author 2'
+                'author_name': 'Author 2',
+                "owner": book_2.owner.id
+
             },
         ]
         self.assertEqual(expected_data, data)
--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
+from store.permissions import IsOwnerOrStaffOrReadOnly
 from store.serializers import BookSerializer
 
 
@@ -12,10 +13,14 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filter_fields = ['price']
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
 
 
 def oath(request):
